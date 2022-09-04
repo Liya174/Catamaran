7585d52b2ed7bbdff503b6b4146a760ce0236e3d
Liia K <lusya-1992@mail.ru>
2022-09-04 21:29:28 +0500

    animation

--- scripts/common.js
+++ scripts/common.js
@@ -1,6 +1,8 @@
 function init() {
     const cards = document.querySelectorAll('.cards-card');
-
+    
+    const entryShownElements = document.querySelectorAll('.entry-shown');
+    
     const onCardHover = (event) => {
         const card = event.target.closest('.cards-card');
         card.querySelector('.cards-card__text').classList.add('active');
@@ -16,10 +18,24 @@ function init() {
         card.addEventListener('mouseover', onCardHover);
         card.removeEventListener('mouseout', onCardOut);
     }
+
+    const onEntry = (entry) => {
+        entry.forEach(change => {
+          if (change.isIntersecting) {
+            change.target.classList.add('active');
+          }
+        });
+      }
     
     Array.from(cards).forEach(card => {
         card.addEventListener('mouseover', onCardHover);
     })
+
+    const options = { threshold: [1] };
+    const observer = new IntersectionObserver(onEntry, options);
+    for (let elm of entryShownElements) {
+        observer.observe(elm);
+    }
 };
 
 document.addEventListener('DOMContentLoaded', init)
