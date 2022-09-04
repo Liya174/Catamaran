149c4c21ad72ffd12b527c18cc43b7fc0b32cff6
Liia K <lusya-1992@mail.ru>
2022-09-04 12:42:21 +0500

    bookModal, feedbackForm

---
+++ scripts/bookModal.js
@@ -0,0 +1,32 @@
+function init() {
+    const modalBook = document.querySelector('#modal-book');
+    const bookButton = document.querySelector('.book-button');
+
+    const showBookModal = () => {
+        modalBook.classList.add('visible');
+    }
+
+    const hideBookModal = () => {
+        modalBook.classList.remove('visible');
+        // TODO: add cleaning the form
+    }
+
+    const onBookButtonClick = () => {
+        showBookModal();
+        bookButton.removeEventListener('click', onBookButtonClick);
+        modalBook.addEventListener('click', onDocumentClick); 
+    }
+
+    const onDocumentClick = () => {
+        const isOutside = !event.target.closest('.modal-book');
+        if (isOutside) {
+            hideBookModal();
+            bookButton.addEventListener('click', onBookButtonClick); 
+            modalBook.removeEventListener('click', onDocumentClick);
+        }
+    }
+
+    bookButton.addEventListener('click', onBookButtonClick);
+};
+
+document.addEventListener('DOMContentLoaded', init)
